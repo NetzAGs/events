eb5c4d74fb9a4a945e8739b4166c96e730d74e14
Florian Hülsmann <fh@cbix.de>
2016-05-29 17:53:27 +0200

    route button structure

--- client/ui/EventsDetail.js
+++ client/ui/EventsDetail.js
@@ -2,9 +2,9 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
 Template.Events_detail.helpers({
-	event() {
+    event() {
         return Events.findOne({_id: FlowRouter.getParam('_id')});
-	},
+    },
     tasks() {
         return Tasks.find({eventId: FlowRouter.getParam('_id')});
     },
@@ -15,4 +15,21 @@ Template.Events_detail.helpers({
     mySlots() {
         return Volunteers.find({userId: Meteor.userId(), 'slot.eventId': FlowRouter.getParam('_id')}, {sort: {'slot.startTime': 1}});
     }
+});
+
+Template.Events_detail.events({
+    'click button.event-admin'(event) {
+        const eventId = FlowRouter.getParam('_id');
+        FlowRouter.go('events.admin', {_id: eventId});
+    },  
+    'click .event-delete': function(event) {
+        event.preventDefault();
+        const eventId = FlowRouter.getParam('_id');
+        bootbox.confirm("Are you sure you want to delete this event?", function(conf) {
+            if(conf) {
+                Meteor.call('events.remove', eventId);
+                FlowRouter.go('events.list');
+            }
+        });
+    },
 });
--- client/ui/EventsListItem.js
+++ client/ui/EventsListItem.js
@@ -1,10 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
+Template.Events_list_item.helpers({
+    eventPath() {
+        return FlowRouter.path('events.detail', this);
+    }
+});
 Template.Events_list_item.events({
-	'click button.event-join'(event) {
-        FlowRouter.go('events.detail', this);
-	},  
 	'click button.event-admin'(event) {
         FlowRouter.go('events.admin', this);
 	},  
